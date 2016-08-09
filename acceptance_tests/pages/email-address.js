91d8e6bbfb205fa1d6ddd02d98d64fb3e7531fdb
Ben Diggle <ben.diggle@transformuk.com>
2016-08-09 10:31:52 +0100

    hmpope-28. Added the email address step to the form. Added the field to the fields config with validation for email. Added the translations for the field which has aconditional hint message. When the user has previously selected My passport application on the /whose-application step, the hint is shown. When they have selected Someone elses application, no hint is shown. Added the acceptance tests for the step, testing the field, labels, hints, validation and forking.

--- acceptance_tests/pages/email-address.js
+++ acceptance_tests/pages/email-address.js
@@ -1,5 +1,27 @@
 'use strict';
 
+const TRANSLATIONS = require('../../apps/track-application/translations/en/default');
+const EMAIL_ADDRESS = TRANSLATIONS.fields['email-address'];
+
+let I;
+
 module.exports = {
-  url: 'email-address'
+
+  _init() {
+    I = require('../steps.js')();
+  },
+
+  url: 'email-address',
+  'email-address': '#email-address',
+  hint: EMAIL_ADDRESS.hint.representative.false,
+  content: {
+    invalid: 'invalid email',
+    valid: 'email@email.com'
+  },
+
+  fillFormAndSubmit(content) {
+    I.seeElement(this['email-address']);
+    I.fillField(this['email-address'], content);
+    I.submitForm();
+  }
 };
